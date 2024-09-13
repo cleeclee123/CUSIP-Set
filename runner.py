a3c7e883146d3116ffbfb4788f48dd7013a88d8f
Chris Lee <cl114@illinois.edu>
2024-09-13 09:36:46 -0500

    2024-09-12 dump

--- runner.py
+++ runner.py
@@ -240,7 +240,7 @@ def parallel_process(dict_df, raw_auctions_df):
 if __name__ == "__main__":
     t1 = time.time()
     start_date = datetime(2024, 9, 10)
-    end_date = datetime(2024, 9, 11)
+    end_date = datetime(2024, 9, 12)
     weeks = get_business_days_groups(start_date, end_date, group_size=20)
     # weeks.reverse()
 
